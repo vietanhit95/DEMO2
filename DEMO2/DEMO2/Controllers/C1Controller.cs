ce757249b2b289df7608c151a57519ffbd223d16
VietAnhNguyen <vietanhit95@gmail.com>
2016-08-13 22:50:46 +0700

    Add Controller

---
+++ DEMO2/DEMO2/Controllers/C1Controller.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DEMO2.Controllers
+{
+    public class C1Controller : Controller
+    {
+        //
+        // GET: /C1/
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Detail()
+        {
+            return View();
+        }
+	}
+}
