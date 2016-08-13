93ba910ddd7e61506bac9f42235d5a1df48315d0
VietAnhNguyen <vietanhit95@gmail.com>
2016-08-13 21:40:11 +0700

    First Demo

---
+++ DEMO2/DEMO2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DEMO2.MvcApplication" Language="C#" %>
---
+++ DEMO2/DEMO2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DEMO2.Startup))]
+namespace DEMO2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
